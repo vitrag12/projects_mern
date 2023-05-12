3f98b4dfb51b8ce144ed2b73f10de267aa5ed150
vitu vitu <124585089+vitrag12@users.noreply.github.com>
2023-05-12 22:24:43 +0530

    Delete 11-Food Delievery directory

--- 11-Food Delievery/client/src/context/alerts/alertState.js
+++
@@ -1,7 +0,0 @@
-import React, { useReducer } from "react";
-import AlertContext from "./AlertContext";
-
-const Alerts = () => {
-  const intitalState = [];
-  const [state, dispatch] = useReducer(AlertReducer, initialState);
-};
--- 11-Food Delievery/client/src/context/auth/removeAuthToken.js
+++
@@ -1,7 +0,0 @@
-import axios from "axios";
-
-const removeAuthToken = () => {
-  delete axios.defaults.headers.common["x-auth-token"];
-};
-
-export default removeAuthToken;
