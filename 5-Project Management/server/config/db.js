de4dcb6dc8f381e451c572f5fa020b7190cd6bb3
vitu vitu <124585089+vitrag12@users.noreply.github.com>
2023-05-12 22:36:56 +0530

    Delete 5-Project Management directory

--- 5-Project Management/server/config/db.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const connectDB = async () => {
-	const conn = await mongoose.connect('mongodb://localhost:27017');
-
-	console.log(`MongoDB Connected ${conn.connection.host}`.cyan.underline.bold);
-};
-
-module.exports = connectDB;
