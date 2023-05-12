e4c49f40d570110465e24ae31c945002ec7da4f3
vitu vitu <124585089+vitrag12@users.noreply.github.com>
2023-05-12 22:34:19 +0530

    Delete 19-News Media directory

--- 19-News Media/front/src/reducer/language.js
+++
@@ -1,7 +0,0 @@
-export default function(langueCode=["fr", "fr"], action) {
-    if(action.type === 'changeLanguage'){
-        return action.langue;
-    } else {
-        return langueCode;
-    }
-}
