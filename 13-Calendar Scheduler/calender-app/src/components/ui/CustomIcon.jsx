a7790e7921725f648c24e2e106820ade2487a182
vitu vitu <124585089+vitrag12@users.noreply.github.com>
2023-05-12 22:28:00 +0530

    Delete 13-Calendar Scheduler directory

--- 13-Calendar Scheduler/calender-app/src/components/ui/CustomIcon.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const CustomIcon = ({ className }) => {
-  return (
-    <>
-      <i className={className}></i>
-    </>
-  );
-};
--- 13-Calendar Scheduler/calender-app/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import { CalenderApp } from './CalenderApp';
-
-import './styles.css';
-
-ReactDOM.render(<CalenderApp />, document.getElementById('root'));
